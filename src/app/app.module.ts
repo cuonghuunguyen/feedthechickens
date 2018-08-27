4b495092171fd25dccd5c58537a5b8b45a9388e8
PN\cuong.nguyen <cuong.nguyen@student.passerellesnumeriques.org>
2018-08-27 10:46:57 +0700

    management 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { RouterModule } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ToastrModule } from 'ngx-Toastr';
-import { AppComponent } from './app.component';
-import { CourseListComponent } from './components/course-list/course-list.component';
-import { CourseEditComponent } from './components/course-edit/course-edit.component';
-import { environment } from '../environments/environment';
-import { CourseServiceService } from './components/shared/course-service.service';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AngularFireModule } from "angularfire2";
+import { AngularFirestoreModule } from "angularfire2/firestore";
+import { AngularFireAuthModule } from "angularfire2/auth";
+import { AngularFireDatabaseModule } from "angularfire2/database";
+import { RouterModule } from "@angular/router";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { ToastrModule } from "ngx-Toastr";
+import { AppComponent } from "./app.component";
+import { FeedingListComponent } from "./components/feeding-list/feeding-list.component";
+import { FeedingEditComponent } from "./components/feeding-edit/feeding-edit.component";
+import { environment } from "../environments/environment";
+import { FeedingService } from "./components/shared/feeding.service";
+import { FeedingManagementComponent } from "./components/feeding-management/feeding-management.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent,
-    CourseEditComponent
+    FeedingListComponent,
+    FeedingEditComponent,
+    FeedingManagementComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFireDatabaseModule,
     FormsModule,
-    ToastrModule.forRoot()
+    ToastrModule.forRoot(),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule // imports firebase/auth, only needed for auth features,
   ],
-  providers: [CourseServiceService],
+  providers: [FeedingService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
